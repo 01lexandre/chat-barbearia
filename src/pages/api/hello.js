aefc980555df7fa3c1e0242e09719c599df103fe
01lexandre <01.lexandre@gmail.com>
2023-01-26 10:11:45 -0300

    teste 12sadfklsdkaflkasdlk

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -6,22 +6,44 @@ import axios from "axios";
 const API_URL = 'https://wpp.treeunfe.com.br'
 export default async function handler(req, res) {
 
-  // // sendMessage({msg: JSON.stringify(req.body)})
-  //
-  // http://localhost:21465/api/{session}/check-connection-session
-  // const sto = storage.getStorage(this, 'authw')
-  const config = {
-    headers: {
-      'Authorization': 'Bearer $2b$10$Jza0bjWgnhO_8fi1KmG.B.lhh7xae2LDGUiRHv8XKJkv.o0qBKlNm',
-      // 'Content-Language': 'pt-BR'
-    }
-  };
-  const data = await axios.post(API_URL+'/api/nw-alexandre/send-message', {
+  // await sendMessage({msg: JSON.stringify(req.body)})
+  // //
+  // const config = {
+  //   headers: {
+  //     'Authorization': 'Bearer $2b$10$Jza0bjWgnhO_8fi1KmG.B.lhh7xae2LDGUiRHv8XKJkv.o0qBKlNm',
+  //     // 'Content-Language': 'pt-BR'
+  //   }
+  // };
+  // const data = await axios.post(API_URL+'/api/nw-alexandre/send-message', {
+  //   "phone": "5544920023965",
+  //   // "phone": "5544998071332",
+  //   "message": JSON.stringify(req.body),
+  //   "isGroup": false
+  // }, config).then((x) => {return x})
+
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", "Bearer $2b$10$Jza0bjWgnhO_8fi1KmG.B.lhh7xae2LDGUiRHv8XKJkv.o0qBKlNm");
+
+  const raw = JSON.stringify({
     "phone": "5544920023965",
     // "phone": "5544998071332",
     "message": JSON.stringify(req.body),
     "isGroup": false
-  }, config).then((x) => {return x})
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch(API_URL+'/api/nw-alexandre/send-message', requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
   res.status(200).json({ re: req.body})
 }
