fd5e9b7be41999f5d2e87887da9eec1109fbaa95
01lexandre <01.lexandre@gmail.com>
2023-01-26 11:28:19 -0300

    com redis 2

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,27 +2,32 @@ import Redis from "ioredis";
 
 const redis = new Redis("rediss://default:AVNS_2quGsE82rY0rewkjm8t@naweby-db-session-do-user-9743412-0.b.db.ondigitalocean.com:25061");
 
-
 const API_URL = 'https://wpp.treeunfe.com.br'
+
 export default async function handler(req, res) {
 
   const data = JSON.parse(req.body)
   let token = null
 
-  console.log(data.session)
-  redis.get(data.session, (err, result) => {
+  console.error('session', data.session)
+
+  await redis.get(data.session, async (err, result) => {
     if (err) {
-      console.error(err);
+      console.error('error', err);
     } else {
-      console.log('aquiii', JSON.parse(result).token); // Prints "value"
-      token = JSON.parse(result).token
+      console.log('aquiii', result); // Prints "value"
+      // token = JSON.parse(result).token
+      token = JSON.parse(result)
     }
   });
 
 
+  console.error('token', token.token)
+
+
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", "Bearer " + token);
+  myHeaders.append("Authorization", "Bearer " + token.token);
 
   const raw = JSON.stringify({
     "phone": "5544920023965",
