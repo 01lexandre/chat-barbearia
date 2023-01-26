ce955fd91cc4ee5dceb91e04bfbf248d41147930
01lexandre <01.lexandre@gmail.com>
2023-01-25 21:56:18 -0300

    teste sa

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,9 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import {sendMessage} from "@/lib/api";
+import axios from "axios";
 
 export default async function handler(req, res) {
 
-  await sendMessage({msg: JSON.stringify(req.body)})
-  res.status(200).json({ re: req.body })
+  // sendMessage({msg: JSON.stringify(req.body)})
+
+  // http://localhost:21465/api/{session}/check-connection-session
+  // const sto = storage.getStorage(this, 'authw')
+  const config = {
+    headers: {
+      'Authorization': 'Bearer $2b$10$VJWa6KLdCgdP6J8_2HIueuh3ljZSHuUJ3DS2Dp1c5lKOajQIbs2bu',
+      // 'Content-Language': 'pt-BR'
+    }
+  };
+  const data = await axios.post(API_URL+'/api/nw-pedro0lexandre/send-message', {
+    "phone": "5544920023965",
+    // "phone": "5544998071332",
+    "message": response.mgs,
+    "isGroup": false
+  }, config).then((x) => {return x})
+  return data.data
+
+
+  res.status(200).json({ re: data.data })
 }
