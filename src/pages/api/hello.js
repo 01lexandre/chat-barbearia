47b8f30917c7bddc68ea7a280996d53bc159ceaa
01lexandre <01.lexandre@gmail.com>
2023-01-25 21:51:36 -0300

    troca de rota 1111222222222

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,7 +2,8 @@
 
 import {sendMessage} from "@/lib/api";
 
-export default function handler(req, res) {
-  sendMessage()
+export default async function handler(req, res) {
+
+  await sendMessage({msg: JSON.stringify(req.body)})
   res.status(200).json({ re: req.body })
 }
